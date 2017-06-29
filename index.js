90ef8205ed682d79f34868bdf6ece255ad48e83d
Alexander Early <alex@npmjs.com>
2017-06-29 14:00:47 -0700

    add nested tests

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const _ = require('lodash')
 const async = require('async')
 const createBenchmark = require('./benchmark')
-const {table} = require('table')
+const {table, getBorderCharacters} = require('table')
 
 const icepick = require('icepick')
 const mori = require('mori')
@@ -34,17 +34,19 @@ setupObjs()
 
 const defaultSetup = (val) => { input = JSON.parse(JSON.stringify(val)) }
 const defaultInputs = Object.keys(inputs)
-
+const nestedInputs = defaultInputs.filter(name => name.match(/Nested$/))
+const randomInt = (max = 10) => (Math.random() * max) | 0
 function randomKey (max = 10) {
-  const randomInt = (Math.random() * max) | 0
-  return `key${randomInt}`
+  return `key${randomInt(max)}`
 }
 
 const ops = [
   'create',
   'access',
   'assoc',
-  'dissoc'
+  'dissoc',
+  'accessNested',
+  'assocNested'
 ]
 
 const libSuites = {
@@ -52,31 +54,41 @@ const libSuites = {
     create: () => input,
     access: () => input[randomKey()],
     assoc: () => { input[randomKey()] = Math.random() },
-    dissoc: () => { delete input[randomKey()] }
+    dissoc: () => { delete input[randomKey()] },
+    accessNested: () => input[randomKey()][randomInt()][randomKey()],
+    assocNested: () => { input[randomKey()][randomInt()][randomKey()] = Math.random() }
   },
   icepick: {
     create: () => icepick.freeze(input),
     access: () => input[randomKey()],
     assoc: () => icepick.assoc(input, randomKey(), Math.random()),
-    dissoc: () => icepick.dissoc(input, randomKey())
+    dissoc: () => icepick.dissoc(input, randomKey()),
+    accessNested: () => input[randomKey()][randomInt()][randomKey()],
+    assocNested: () => icepick.assocIn(input, [randomKey(), randomInt(), randomKey()], Math.random())
   },
   seamless: {
     create: () => seamless(input),
     access: () => input[randomKey()],
     assoc: () => seamless.set(input, randomKey(), Math.random()),
-    dissoc: () => seamless.without(input, randomKey())
+    dissoc: () => seamless.without(input, randomKey()),
+    accessNested: () => input[randomKey()][randomInt()][randomKey()],
+    assocNested: () => seamless.setIn(input, [randomKey(), randomInt(), randomKey()], Math.random())
   },
   Immutable: {
     create: () => Immutable.fromJS(input),
     access: () => input.get(randomKey()),
     assoc: () => input.set(randomKey(), Math.random()),
-    dissoc: () => input.delete(randomKey())
+    dissoc: () => input.delete(randomKey()),
+    accessNested: () => input.getIn([randomKey(), randomInt(), randomKey()]),
+    assocNested: () => input.setIn([randomKey(), randomInt(), randomKey()], Math.random())
   },
   mori: {
     create: () => mori.toClj(input),
     access: () => mori.get(input, randomKey()),
     assoc: () => mori.assoc(input, randomKey(), Math.random()),
-    dissoc: () => mori.dissoc(input, randomKey())
+    dissoc: () => mori.dissoc(input, randomKey()),
+    accessNested: () => mori.getIn(input, [randomKey(), randomInt(), randomKey()]),
+    assocNested: () => mori.assocIn(input, [randomKey(), randomInt(), randomKey()], Math.random())
 
   }/*,
   Baobab: {
@@ -95,6 +107,9 @@ const testDefs = _.flatten(
           defaultSetup(val)
           input = suite.create()
         },
+      testInputs: op.match(/Nested$/)
+        ? nestedInputs
+        : defaultInputs,
       fn: suite[op] || (() => {})
     }))
   })
@@ -102,11 +117,6 @@ const testDefs = _.flatten(
 
 function createSuites () {
   const tests = testDefs
-    .map(testDef => {
-      testDef.setup = testDef.setup || defaultSetup
-      testDef.testInputs = testDef.inputs ? testDef.inputs.split() : defaultInputs
-      return testDef
-    })
     .reduce((list, testDef) => {
       const {
         library,
@@ -137,11 +147,10 @@ function createSuites () {
 
 function createSuite (type, tests) {
   return function run (cb) {
-    console.log(type)
+    console.log(`${type} (µs)`)
     async.mapSeries(tests, (test, next) => {
       const bench = createBenchmark(test)
       const {mean, stdDev} = bench.run()
-      //console.log(`${test.name}: ${mean.toFixed(3)}µs per run (±${stdDev.toFixed(3)})`)
       async.setImmediate(next, null, _.assign({mean, stdDev}, test))
     }, (err, results) => {
       if (err) throw err
@@ -162,9 +171,14 @@ const libs = _.keys(libSuites)
 function resultTable (results) {
   const rows = libs.map(library => {
     const means = _.map(tableHeader.slice(1), inputName => {
-      return _.find(results, {inputName, library}).mean.toFixed(3)
+      const result = _.find(results, {inputName, library})
+      if (!result) return 'N/A'
+      return result.mean.toFixed(3)
     })
     return [library, ...means]
   })
-  console.log(table([tableHeader, ...rows]))
+  console.log(table([tableHeader, ...rows], {
+    border: getBorderCharacters(`norc`),
+    drawHorizontalLine: (index, size) => index === 1 || index === 0 || index === size
+  }))
 }
