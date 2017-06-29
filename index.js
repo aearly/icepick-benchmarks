73ba7476b2c16a07cbf4f92fa3c6c5acbec4a1c8
Alexander Early <alex@npmjs.com>
2017-06-29 15:21:37 -0700

    chore: move tests to separate file

--- index.js
+++ index.js
@@ -3,13 +3,7 @@ const async = require('async')
 const createBenchmark = require('./benchmark')
 const {table, getBorderCharacters} = require('table')
 
-const icepick = require('icepick')
-const mori = require('mori')
-const Immutable = require('immutable')
-const seamless = require('seamless-immutable')
-
 const inputs = {}
-let input
 
 const objify = (createVal = () => Math.random()) => (obj, i) => {
   obj[`key${i}`] = createVal()
@@ -31,13 +25,8 @@ function setupObjs () {
 
 setupObjs()
 
-const defaultSetup = (val) => { input = JSON.parse(JSON.stringify(val)) }
-const defaultInputs = Object.keys(inputs)
+const defaultInputs = _.keys(inputs)
 const nestedInputs = defaultInputs.filter(name => name.match(/Nested$/))
-const randomInt = (max = 10) => (Math.random() * max) | 0
-function randomKey (max = 10) {
-  return `key${randomInt(max)}`
-}
 
 const ops = [
   'create',
@@ -49,65 +38,14 @@ const ops = [
   'thaw'
 ]
 
-const libSuites = {
-  vanilla: {
-    create: () => input,
-    access: () => input[randomKey()],
-    assoc: () => { input[randomKey()] = Math.random() },
-    dissoc: () => { delete input[randomKey()] },
-    accessNested: () => input[randomKey()][randomInt()][randomKey()],
-    assocNested: () => { input[randomKey()][randomInt()][randomKey()] = Math.random() },
-    thaw: () => input
-  },
-  icepick: {
-    create: () => icepick.freeze(input),
-    access: () => input[randomKey()],
-    assoc: () => icepick.assoc(input, randomKey(), Math.random()),
-    dissoc: () => icepick.dissoc(input, randomKey()),
-    accessNested: () => input[randomKey()][randomInt()][randomKey()],
-    assocNested: () => icepick.assocIn(input, [randomKey(), randomInt(), randomKey()], Math.random()),
-    thaw: () => icepick.thaw(input)
-  },
-  seamless: {
-    create: () => seamless(input),
-    access: () => input[randomKey()],
-    assoc: () => seamless.set(input, randomKey(), Math.random()),
-    dissoc: () => seamless.without(input, randomKey()),
-    accessNested: () => input[randomKey()][randomInt()][randomKey()],
-    assocNested: () => seamless.setIn(input, [randomKey(), randomInt(), randomKey()], Math.random()),
-    thaw: () => seamless.asMutable(input, {deep: true})
-  },
-  Immutable: {
-    create: () => Immutable.fromJS(input),
-    access: () => input.get(randomKey()),
-    assoc: () => input.set(randomKey(), Math.random()),
-    dissoc: () => input.delete(randomKey()),
-    accessNested: () => input.getIn([randomKey(), randomInt(), randomKey()]),
-    assocNested: () => input.setIn([randomKey(), randomInt(), randomKey()], Math.random()),
-    thaw: () => input.toJS()
-  },
-  mori: {
-    create: () => mori.toClj(input),
-    access: () => mori.get(input, randomKey()),
-    assoc: () => mori.assoc(input, randomKey(), Math.random()),
-    dissoc: () => mori.dissoc(input, randomKey()),
-    accessNested: () => mori.getIn(input, [randomKey(), randomInt(), randomKey()]),
-    assocNested: () => mori.assocIn(input, [randomKey(), randomInt(), randomKey()], Math.random()),
-    thaw: () => mori.toJs(input)
-  }
-}
+const {libSuites, defaultSetup} = require('./tests')
 
 const testDefs = _.flatten(
   _.map(libSuites, (suite, library) => {
     return ops.map(op => ({
       library,
       type: op,
-      setup: op === 'create'
-        ? defaultSetup
-        : (val) => {
-          defaultSetup(val)
-          input = suite.create()
-        },
+      setup: defaultSetup(suite, op),
       testInputs: op.match(/Nested$/)
         ? nestedInputs
         : defaultInputs,
---
+++ tests.js
@@ -0,0 +1,71 @@
+
+const icepick = require('icepick')
+const mori = require('mori')
+const Immutable = require('immutable')
+const seamless = require('seamless-immutable')
+
+let input
+
+const randomInt = (max = 10) => (Math.random() * max) | 0
+function randomKey (max = 10) {
+  return `key${randomInt(max)}`
+}
+
+const simpleClone = (val) => { input = JSON.parse(JSON.stringify(val)) }
+exports.defaultSetup = (suite, op) => {
+  if (op === 'create') {
+    return simpleClone
+  }
+  return (val) => {
+    simpleClone(val)
+    input = suite.create()
+  }
+}
+
+exports.libSuites = {
+  vanilla: {
+    create: () => input,
+    access: () => input[randomKey()],
+    assoc: () => { input[randomKey()] = Math.random() },
+    dissoc: () => { delete input[randomKey()] },
+    accessNested: () => input[randomKey()][randomInt()][randomKey()],
+    assocNested: () => { input[randomKey()][randomInt()][randomKey()] = Math.random() },
+    thaw: () => input
+  },
+  icepick: {
+    create: () => icepick.freeze(input),
+    access: () => input[randomKey()],
+    assoc: () => icepick.assoc(input, randomKey(), Math.random()),
+    dissoc: () => icepick.dissoc(input, randomKey()),
+    accessNested: () => input[randomKey()][randomInt()][randomKey()],
+    assocNested: () => icepick.assocIn(input, [randomKey(), randomInt(), randomKey()], Math.random()),
+    thaw: () => icepick.thaw(input)
+  },
+  seamless: {
+    create: () => seamless(input),
+    access: () => input[randomKey()],
+    assoc: () => seamless.set(input, randomKey(), Math.random()),
+    dissoc: () => seamless.without(input, randomKey()),
+    accessNested: () => input[randomKey()][randomInt()][randomKey()],
+    assocNested: () => seamless.setIn(input, [randomKey(), randomInt(), randomKey()], Math.random()),
+    thaw: () => seamless.asMutable(input, {deep: true})
+  },
+  Immutable: {
+    create: () => Immutable.fromJS(input),
+    access: () => input.get(randomKey()),
+    assoc: () => input.set(randomKey(), Math.random()),
+    dissoc: () => input.delete(randomKey()),
+    accessNested: () => input.getIn([randomKey(), randomInt(), randomKey()]),
+    assocNested: () => input.setIn([randomKey(), randomInt(), randomKey()], Math.random()),
+    thaw: () => input.toJS()
+  },
+  mori: {
+    create: () => mori.toClj(input),
+    access: () => mori.get(input, randomKey()),
+    assoc: () => mori.assoc(input, randomKey(), Math.random()),
+    dissoc: () => mori.dissoc(input, randomKey()),
+    accessNested: () => mori.getIn(input, [randomKey(), randomInt(), randomKey()]),
+    assocNested: () => mori.assocIn(input, [randomKey(), randomInt(), randomKey()], Math.random()),
+    thaw: () => mori.toJs(input)
+  }
+}
